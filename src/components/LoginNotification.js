0fdf877df03bed5855626f39c66f92c044e2d650
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-21 04:30:17 +0100

    close btn removed

--- src/components/LoginNotification.js
+++ src/components/LoginNotification.js
@@ -1,31 +1,15 @@
 import * as React from 'react';
-import { Box, Collapse, IconButton, Alert } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import { Box, Collapse, Alert } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
 
 const LoginNotification = () => {
   const { setAlert } = useAuth();
-
-  const closeNotification = () => {
-    // Set isAlert to false when the notification is closed
-    setAlert((prevAlert) => ({ ...prevAlert, isAlert: false }));
-  };
 
   return (
     <Box sx={{ mb: 2 }}>
       <Collapse in={true}>
         <Alert
           severity="info"
-          action={
-            <IconButton
-              aria-label="Close"
-              size="small"
-              onClick={closeNotification}
-            >
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          }
-          onClose={closeNotification} 
         >
           Kindly Log In to view images clearly.
         </Alert>
