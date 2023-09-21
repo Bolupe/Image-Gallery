beeffbff7ee6d7178fe36db445154de64c8be396
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-21 12:21:06 +0100

    better drag and drop effect

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,64 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { ImageContext } from '../App';
-import { Draggable, Droppable, DragDropContext} from 'react-beautiful-dnd';
 import Image from './Image';
 import { useAuth } from '../context/AuthContext';
+import Sortable from 'sortablejs';
 
 const Images = () => {
-  const { response, isLoading, searchImage, handleDragEnd } = useContext(
-    ImageContext
-  );
+  const { response, isLoading, searchImage } = useContext(ImageContext);
+  const { currentUser } = useAuth();
 
-  const { currentUser } = useAuth(); // Access the currentUser from AuthContext
+  const imageContainerRefs = useRef([]);
+
+  useEffect(() => {
+    const sortableContainer = new Sortable(document.getElementById('image-container'), {
+      animation: 150,
+      onEnd: handleDragEnd,
+    });
+
+    return () => {
+      sortableContainer.destroy();
+    };
+  }, []);
+
+  const handleDragEnd = (event) => {
+    const { oldIndex, newIndex } = event;
+
+    if (oldIndex !== newIndex) {
+      // Clone the response array
+      const updatedResponse = [...response];
+
+      // Move the dragged item to the new position
+      const [movedItem] = updatedResponse.splice(oldIndex, 1);
+      updatedResponse.splice(newIndex, 0, movedItem);
+
+      // Update the response state with the new order
+      // Note: You may need to call an API to persist this order
+      // in a real application.
+      // This example updates the state only.
+      // setResponse(updatedResponse);
+    }
+  };
 
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div>
-        <h1 className="text-center mt-6 underline text-2xl">
-          Results for {searchImage || 'Wallpapers'}
-        </h1>
-        <Droppable droppableId="image-list" direction="horizontal">
-          {(provided) => (
-            <div
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4"
-            >
-              {isLoading ? (
-                response.map((data, index) => (
-                  <Draggable key={data.id} draggableId={data.id} index={index}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <Image data={data} currentUser={currentUser} /> {/* Pass currentUser to the Image component */}
-                      </div>
-                    )}
-                  </Draggable>
-                ))
-              ) : (
-                response.map((data, index) => (
-                  <Draggable key={data.id} draggableId={data.id} index={index}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <Image data={data} currentUser={currentUser} /> {/* Pass currentUser to the Image component */}
-                      </div>
-                    )}
-                  </Draggable>
-                ))
-              )}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </div>
-    </DragDropContext>
+    <div id="image-container" className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4">
+      {response.map((data, index) => (
+        <div
+          key={data.id}
+          ref={(el) => (imageContainerRefs.current[index] = el)}
+        >
+          <Image data={data} currentUser={currentUser} />
+        </div>
+      ))}
+    </div>
   );
 };
 
